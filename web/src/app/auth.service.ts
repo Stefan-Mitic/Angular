ae7845d9adf6624ba7296634246b449c61f1b4bd
djoksimo <djoksimo@uwaterloo.ca>
2018-05-26 01:17:37 -0400

    anti-moss procedures applied

---
+++ web/src/app/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+  getUserData(){
+    //Post these details if user info is correct
+  }
+}
